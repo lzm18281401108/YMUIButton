6def70f062f548d8a486b0adcd962c57e0628ee1
zhiming liu <zhimingliu@zhimingdeiMac.local>
2018-01-24 16:20:17 +0800

    first commit

---
+++ YMUIbutton/YMButton.m
@@ -0,0 +1,21 @@
+//
+//  YMButton.m
+//  YMUIbutton
+//
+//  Created by zhiming liu on 2018/1/24.
+//  Copyright © 2018年 zhiming liu. All rights reserved.
+//
+
+#import "YMButton.h"
+
+@implementation YMButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
